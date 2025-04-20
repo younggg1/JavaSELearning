682af600f00012d246b358222fa4d93f845618d7
younggg1 <1064254619@qq.com>
2025-04-20 15:21:05 +0800

    泛型

---
+++ module_Genericity/src/com/javase/genercity/Demo04Genericity.java
@@ -0,0 +1,11 @@
+package com.javase.genercity;
+
+public class Demo04Genericity {
+    public static void main(String[] args) {
+        MyArrayList1<String> list1 = new MyArrayList1<>();
+        list1.add("张三");
+        list1.add("李四");
+        System.out.println(list1.get(0));
+
+    }
+}
